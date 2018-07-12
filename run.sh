d128714b169ccb4b8e960e5eb94911f84ef40f62
ysicing <i@spanda.io>
2018-07-12 17:51:56 +0800

    fix typo

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PORT=${PORT:-5000}
-
 sleep ${PAUSE:-0}
 
-exec java $JAVA_OPTS -jar target/dependency/webapp-runner.jar   --port 5000  target/*.war 
+exec java $JAVA_OPTS -jar target/dependency/webapp-runner.jar   --port ${PORT:-5000}  target/*.war 
